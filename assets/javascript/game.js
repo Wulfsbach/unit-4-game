6ba909955691a6136904dcf88b6965163ec45cdd
Wulfsbach <etemelkos@gmail.com>
2018-05-01 18:57:46 -0400

    here

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+$function()
+//The Fellowship
+//Gimli Object
+var gimli = {
+    Name = "Gimli",
+    health = 150,
+    attack= 5,
+    image: '<img src="./assets/images/placeholder.png">'
+}
+
+//Legolas Object
+
+var Legolas ={
+    name: "Legolas",
+    health: 110,
+    attack: 7,
+    image: '<img src=+>',
+
+}
+//Aragorn Object
+var Aragorn ={
+    name: "Aragorn",
+    health: 100,
+    attack: 10,
+
+}
+//Frodo Object
+var Frodo ={
+    health: 60,
+    attack: 3,
+}
